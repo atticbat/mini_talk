c99a93d4a5ab53f505e9d96cd76c0131a11f0262
Kewin Hatlas <khatlas@2-G-3.42heilbronn.de>
2022-08-10 04:12:00 +0200

    removed compiled files

--- obj/client.d
+++
@@ -1,8 +0,0 @@
-obj/client.o: mandatory/client.c includes/mini_talk.h \
-  includes/ft_printf.h includes/libft.h
-
-includes/mini_talk.h:
-
-includes/ft_printf.h:
-
-includes/libft.h:
--- obj/server.d
+++
@@ -1,8 +0,0 @@
-obj/server.o: mandatory/server.c includes/mini_talk.h \
-  includes/ft_printf.h includes/libft.h
-
-includes/mini_talk.h:
-
-includes/ft_printf.h:
-
-includes/libft.h:
